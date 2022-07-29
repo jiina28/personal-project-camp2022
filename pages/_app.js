08f378095577e250ed965c2360919b1e2d82be69
jiina28 <jiina_28@naver.com>
2022-07-29 17:07:06 +0900

    done without info page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import {config} from '@fortawesome/fontawesome-svg-core'
+config.autoAddCss = false
+
+import Layout from "../components/sidebar";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+    <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
